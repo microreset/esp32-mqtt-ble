8fdbfef3b65b01ab3a15644e8b11f9effa0f4e79
Mimi Portable <crosetremi@gmail.com>
2018-01-27 15:45:11 -0500

    Formated MQTT topic and payload to be compatible with home assistant

--- src/main.ino
+++ src/main.ino
@@ -15,6 +15,7 @@
 */
 typedef struct {
   String  address;
+  int16_t rssi;
   bool    isDiscovered;
   long    lastDiscovery;
   bool    isInZone;
@@ -52,6 +53,7 @@ class MyAdvertisedDeviceCallbacks:
             BLETrackedDevices[i].isDiscovered = true;
             BLETrackedDevices[i].lastDiscovery = millis();
             BLETrackedDevices[i].toNotify = true;
+            BLETrackedDevices[i].rssi = advertisedDevice.getRSSI();
 
             DEBUG_PRINT(F("INFO: Tracked device newly discovered, Address: "));
             DEBUG_PRINT(advertisedDevice.getAddress().toString().c_str());
@@ -59,6 +61,8 @@ class MyAdvertisedDeviceCallbacks:
             DEBUG_PRINTLN(advertisedDevice.getRSSI());
           } else { // Device was already discovered:
             BLETrackedDevices[i].lastDiscovery = millis();
+            BLETrackedDevices[i].rssi = advertisedDevice.getRSSI();
+
             DEBUG_PRINT(F("INFO: Tracked device discovered, Address: "));
             DEBUG_PRINT(advertisedDevice.getAddress().toString().c_str());
             DEBUG_PRINT(F(", RSSI: "));
@@ -73,7 +77,7 @@ class MyAdvertisedDeviceCallbacks:
               } else { // and it was already:
                 BLETrackedDevices[i].toNotify = false;
               }
-              DEBUG_PRINT(F("INFO: Device position: In the "));
+              DEBUG_PRINT(F("INFO: Device position: "));
               DEBUG_PRINTLN(F(LOCATION));
             }
             // If out the proximity zone:
@@ -85,7 +89,7 @@ class MyAdvertisedDeviceCallbacks:
               } else { // and it was already:
                 BLETrackedDevices[i].toNotify = false;
               }
-              DEBUG_PRINT(F("INFO: Device position: Outside the "));
+              DEBUG_PRINT(F("INFO: Device position: "));
               DEBUG_PRINTLN(F(LOCATION));
             }
           }
@@ -104,7 +108,10 @@ class MyAdvertisedDeviceCallbacks:
 ///////////////////////////////////////////////////////////////////////////
 volatile unsigned long lastMQTTConnection = 0;
 char MQTT_CLIENT_ID[7] = {0};
+char BLE_ADDRESS[17] = {0};
 char MQTT_AVAILABILITY_TOPIC[sizeof(MQTT_CLIENT_ID) + sizeof(MQTT_AVAILABILITY_TOPIC_TEMPLATE) - 2] = {0};
+char MQTT_RSSI[4] = {0};
+char MQTT_SENSOR_PAYLOAD[sizeof(MQTT_SENSOR_PAYLOAD_TEMPLATE) + sizeof(MQTT_CLIENT_ID) + sizeof(MQTT_RSSI) - 2] = {0};
 /*
   Function called to publish to a MQTT topic with the given payload
 */
@@ -129,7 +136,7 @@ void connectToMQTT() {
     if (lastMQTTConnection < millis()) {
       if (mqttClient.connect(MQTT_CLIENT_ID, MQTT_USERNAME, MQTT_PASSWORD, MQTT_AVAILABILITY_TOPIC, 0, 1, MQTT_PAYLOAD_UNAVAILABLE)) {
         DEBUG_PRINTLN(F("INFO: The client is successfully connected to the MQTT broker"));
-        // publishToMQTT(MQTT_AVAILABILITY_TOPIC, MQTT_PAYLOAD_AVAILABLE);
+        publishToMQTT(MQTT_AVAILABILITY_TOPIC, MQTT_PAYLOAD_AVAILABLE);
       } else {
         DEBUG_PRINTLN(F("ERROR: The connection to the MQTT broker failed"));
         DEBUG_PRINT(F("INFO: MQTT username: "));
@@ -165,17 +172,11 @@ void setup() {
   DEBUG_PRINT(F("INFO: MQTT availability topic: "));
   DEBUG_PRINTLN(MQTT_AVAILABILITY_TOPIC);
 
-  char mqttTopic[sizeof(MQTT_CLIENT_ID) + sizeof(MQTT_SENSOR_TOPIC_TEMPLATE) + sizeof(LOCATION) + 12 - 4] = {0};
+  // presence/mimibed
+  // sizeof(MQTT_CLIENT_ID)
+  char mqttTopic[sizeof(MQTT_SENSOR_TOPIC_TEMPLATE) + sizeof(LOCATION) - 4] = {0};
   for (uint8_t i = 0; i < NB_OF_BLE_TRACKED_DEVICES; i++) {
-      char tmp_ble_address[13] = {0};
-      String tmp_string_ble_address = BLETrackedDevices[i].address;
-      tmp_string_ble_address.replace(":", "");
-      tmp_string_ble_address.toCharArray(tmp_ble_address, sizeof(tmp_ble_address));
-      // sprintf(mqttTopic, MQTT_SENSOR_TOPIC_TEMPLATE, MQTT_CLIENT_ID, LOCATION, tmp_ble_address);
-      // MQTT binary sensor: <CHIP_ID>/sensor/<LOCATION>/<BLE_ADDRESS>
-      // MQTT binary sensor: /presence/<LOCATION>/<BLE_ADDRESS>
-      // /sensor/bedroom/e092e56930ef/state AWAY
-      sprintf(mqttTopic, MQTT_SENSOR_TOPIC_TEMPLATE, LOCATION, tmp_ble_address);
+      sprintf(mqttTopic, MQTT_SENSOR_TOPIC_TEMPLATE, LOCATION);
       memcpy(BLETrackedDevices[i].mqttTopic, mqttTopic, sizeof(mqttTopic) + 1);
       DEBUG_PRINT(F("INFO: MQTT sensor topic: "));
       DEBUG_PRINTLN(BLETrackedDevices[i].mqttTopic);
@@ -220,10 +221,16 @@ void loop() {
 
     for (uint8_t i = 0; i < NB_OF_BLE_TRACKED_DEVICES; i++) {
       if (BLETrackedDevices[i].toNotify) {
-        if (BLETrackedDevices[i].isDiscovered && BLETrackedDevices[i].isInZone) {
-            publishToMQTT(BLETrackedDevices[i].mqttTopic, MQTT_PAYLOAD_ON);
-        } else if (BLETrackedDevices[i].isDiscovered && !BLETrackedDevices[i].isInZone) {
-          publishToMQTT(BLETrackedDevices[i].mqttTopic, MQTT_PAYLOAD_OFF);
+
+        String tmp_string_ble_address = BLETrackedDevices[i].address;
+        tmp_string_ble_address.toCharArray(BLE_ADDRESS, sizeof(BLE_ADDRESS));
+
+        sprintf(MQTT_RSSI, "%d", BLETrackedDevices[i].rssi);
+
+        sprintf(MQTT_SENSOR_PAYLOAD, MQTT_SENSOR_PAYLOAD_TEMPLATE, BLE_ADDRESS, MQTT_RSSI);
+
+        if (BLETrackedDevices[i].isDiscovered) {
+          publishToMQTT(BLETrackedDevices[i].mqttTopic, MQTT_SENSOR_PAYLOAD);
         } else {
           publishToMQTT(BLETrackedDevices[i].mqttTopic, MQTT_PAYLOAD_AWAY);
         }
